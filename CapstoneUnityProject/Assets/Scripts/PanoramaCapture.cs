4819edcd69a2289930706a7c5a4e735570c29a08
John Keane <jrkeane1@asu.edu>
2021-01-16 17:44:02 -0700

    Inital Unity project
    A defualt 3d unity project with some assests from the unity store and some scripts I have previously written. Some assests are too large and will need to be downloaded indiviually in order to successfully import this project locally.

---
+++ CapstoneUnityProject/Assets/Scripts/PanoramaCapture.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Code taken from https://www.youtube.com/watch?v=nodjrfARFA0
+public class PanoramaCapture : MonoBehaviour
+{
+    public Camera targetCamera;
+    public RenderTexture cubeMapLeft;
+    public RenderTexture rt;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            capture();
+    }
+    public void capture()
+    {
+        targetCamera.RenderToCubemap(cubeMapLeft);
+        cubeMapLeft.ConvertToEquirect(rt);
+        Save(rt);
+    }
+
+    public void Save(RenderTexture t)
+    {
+        Texture2D tex = new Texture2D(t.width, t.height);
+
+        RenderTexture.active = t;
+        tex.ReadPixels(new Rect(0, 0, t.width, t.height),0,0);
+        RenderTexture.active = null;
+        byte[] bytes = tex.EncodeToJPG();
+        string path = Application.dataPath + "/PanoramaTest" + ".jpg";
+        System.IO.File.WriteAllBytes(path, bytes);
+    }
+}
---
+++ CapstoneUnityProject/Assets/Scripts/SpawnObjects.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Some code taken from https://answers.unity.com/questions/714835/best-way-to-spawn-prefabs-in-a-circle.html
+public class SpawnObjects : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int numObjects = 10;
+    public int numRows = 10;
+    public GameObject prefab;
+ 
+    void Start()
+    {
+        int currentNumObjects = numObjects;
+        int numSpawned = numObjects;
+        Vector3 center = transform.position;
+        for (int j = 0; j < numRows; j++)
+        {
+            for (int i = 0; i < currentNumObjects; i++)
+            {
+                float radius = j * 1.15f + 4;
+                Vector3 pos = RandomCircle(center, radius);
+                Quaternion rot = Quaternion.FromToRotation(Vector3.forward, center - pos);
+                Instantiate(prefab, pos, rot);
+            }
+            currentNumObjects = numObjects * (j + 1);
+            numSpawned += currentNumObjects;
+        }
+        Debug.Log(numSpawned);
+    }
+    
+    Vector3 RandomCircle( Vector3 center, float radius)
+    {
+        float angle = Random.value * 360;
+        Vector3 pos;
+        pos.x = center.x + radius * Mathf.Sin(angle * Mathf.Deg2Rad);
+        pos.z = center.z + radius * Mathf.Cos(angle * Mathf.Deg2Rad);
+        pos.y = center.y;
+        return pos;
+    }
+}
